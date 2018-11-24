dd71f1e0e0d4be328a0323d4d5df44853a769125
avillanova <felipe.godinho@primecontrol.com.br>
2018-11-24 14:43:36 -0200

    commit geral

--- src/main/java/util/RunnerTest.java
+++ src/main/java/util/RunnerTest.java
@@ -1,4 +1,13 @@
 package util;
 
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "TrelloUI.feature"
+)
 public class RunnerTest {
+
 }
